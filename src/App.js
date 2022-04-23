a96f2ad8c34b118e1fc0824158d65d08eff3b3f8
codevibek <vibek2925@gmail.com>
2022-04-23 18:02:44 +0545

    create basic ecommerce frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar.js'
+import Productlist from './components/Productlist.js'
+import Default from './components/Default.js'
+import Details from './components/Details.js'
+import Cart from './components/Cart.js'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+          <Switch>
+            <Route path="/" exact component={Productlist}/>
+            <Route path="/details" component={Details}/>
+            <Route path="/cart" component={Cart}/>
+            <Route  component={Default}/>
+          </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "apple",
+    price: 20,
+    img: "/seu.jpg",
+    company: "",
+    info: "",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: "apple",
+    price: 20,
+    img: "/phone.jpg",
+    company: "",
+    info: "",
+    inCart: "",
+    count: 0,
+    total: 0,
+  },
+];
+
+export const detailProduct = {
+  id: 3,
+  title: "Iphone 12",
+  img: "",
+  company: "Apple",
+  info: "jkahskj k kjah kjjf i hiu hjiu i hfiuhi",
+  inCart: false,
+  count: 0,
+  total: 0,
+};
